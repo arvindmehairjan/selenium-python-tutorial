c26a91ae38ff73241ced949796e8141a9564f901
despi <despi@LAPTOP-JL8GAFLU>
2020-04-11 20:09:13 +0200

    test

---
+++ selenium_python.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time
+
+# Open Chrome Browser using Chromedriver
+driver = webdriver.Chrome("chromedriver.exe")
+
+# Navigate to python.org
+driver.get("https://www.python.org/")
+time.sleep(3)
+
+# Enter text search bar
+enter_searchbar = driver.find_element_by_id("id-search-field")
+enter_searchbar.send_keys("loops")
+
+# Click on the button to display results
+click_button = driver.find_element_by_id("submit")
+click_button.click()
+
+time.sleep(3)
+
+# Close Browser 
+driver.close()
